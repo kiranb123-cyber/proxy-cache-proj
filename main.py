2110ddbd7c0b8548decf70e558d302b2beda71fb
Kiran Bhatt <kiranbhatt715@gmail.com>
2026-05-31 21:08:22 +0100

    feature: added clear caching

--- main.py
+++ main.py
@@ -7,19 +7,29 @@ app = Flask(__name__)
 
 #the local cache for now
 cache = {}
+def cache_clear():
+    cache.clear()
+
+
 
 #cli section
 #port cli
 parser = argparse.ArgumentParser(description="proxy's cache")
 parser.add_argument("--port", type=int, help="put in your port number")
 parser.add_argument("--origin", type=str, help="put in your url server like dummy json")
+parser.add_argument("--clear-cache", help="clears cache", action="store_true")
 args = parser.parse_args()
+
+#esnures cache is empty before its going to be used repeatedly
+if args.clear_cache:
+    cache_clear()
+    exit()
+
 
 
 #what catches every path and applies it to the dummy route
 @app.route("/", defaults={"Allpaths":""})
 @app.route("/<path:Allpaths>")
-
 #the function that carries it all out
 def catch_all(Allpaths):
     #checks if its in the cache
@@ -38,6 +48,7 @@ def catch_all(Allpaths):
         custom_response.headers["X-Cache"] = "HIT"
         return custom_response
     
+
 
 
 #this is for the cli --port 9000
